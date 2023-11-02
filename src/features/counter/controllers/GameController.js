43579dae1d2f62506909d8b61fc7129c2dabfa61
Pngshka <pngshkapng@gmail.com>
2023-11-03 00:06:27 +0300

    Что-то

--- src/features/counter/controllers/GameController.js
+++ src/features/counter/controllers/GameController.js
@@ -1,4 +1,4 @@
-import * as PIXI from 'pixi.js'
+//import * as PIXI from 'pixi.js'
 import {BallController} from './BallController.js'
 import {AnimationController} from './AnimationController'
 
@@ -16,58 +16,58 @@ export default class GameController {
     }
 
     async loadingManifest() {
-        // var request = new XMLHttpRequest();
-        // request.open("GET", "./pixiAssets.json");
-        // request.responseType = "json";
-        // request.send();
-        // request.onload = function () {
-        //     var test = request.response;
-        //     console.log(test["speed"]);
-        // };
 
-        this.atlasData = {
-            frames: {
-                animations1: {
-                    frame: { x: 138, y: 16, w: 174, h: 174 },
-                    sourceSize: { w: 174, h: 174 },
-                    spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
-                },
-                animations2: {
-                    frame: { x: 579, y: 47, w: 174, h: 174 },
-                    sourceSize: { w: 174, h: 174 },
-                    spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
-                },
-                animations3: {
-                    frame: { x: 1016, y: 169, w: 174, h: 174 },
-                    sourceSize: { w: 174, h: 174 },
-                    spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
-                },
-                animations4: {
-                    frame: { x: 1441, y: 278, w: 211, h: 121 },
-                    sourceSize: { w: 211, h: 121 },
-                    spriteSourceSize: { x: 0, y: 0, w: 211, h: 121 }
-                },
-                animations5: {
-                    frame: { x: 1910, y: 154, w: 158, h: 183 },
-                    sourceSize: { w: 158, h: 183 },
-                    spriteSourceSize: { x: 0, y: 0, w: 158, h: 183 }
-                },
-                animations6: {
-                    frame: { x: 2339, y: 56, w: 174, h: 174 },
-                    sourceSize: { w: 174, h: 174 },
-                    spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
-                },
-            },
-            meta: {
-                image: '../photo.png',
-                format: 'RGBA8888',
-                size: { w: 2640, h: 440 },
-                scale: 1
-            },
-            animations: {
-                enemy: ['animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1'] //array of frames by name
-            }
-        }
+        const response = await fetch('/pixiAssetsMy.json')
+        const data = await response.json();
+        
+        const response2 = await fetch('/pixiAtlas.json')
+        this.atlasData = await response2.json();
+
+        // this.atlasData = {
+        //     frames: {
+        //         animations1: {
+        //             frame: { x: 138, y: 16, w: 174, h: 174 },
+        //             sourceSize: { w: 174, h: 174 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
+        //         },
+        //         animations2: {
+        //             frame: { x: 579, y: 47, w: 174, h: 174 },
+        //             sourceSize: { w: 174, h: 174 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
+        //         },
+        //         animations3: {
+        //             frame: { x: 1016, y: 169, w: 174, h: 174 },
+        //             sourceSize: { w: 174, h: 174 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
+        //         },
+        //         animations4: {
+        //             frame: { x: 1441, y: 278, w: 211, h: 121 },
+        //             sourceSize: { w: 211, h: 121 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 211, h: 121 }
+        //         },
+        //         animations5: {
+        //             frame: { x: 1910, y: 154, w: 158, h: 183 },
+        //             sourceSize: { w: 158, h: 183 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 158, h: 183 }
+        //         },
+        //         animations6: {
+        //             frame: { x: 2339, y: 56, w: 174, h: 174 },
+        //             sourceSize: { w: 174, h: 174 },
+        //             spriteSourceSize: { x: 0, y: 0, w: 174, h: 174 }
+        //         },
+        //     },
+        //     meta: {
+        //         image: '../photo.png',
+        //         format: 'RGBA8888',
+        //         size: { w: 2640, h: 440 },
+        //         scale: 1
+        //     },
+        //     animations: {
+        //         enemy: ['animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1', 'animations2', 'animations3', 'animations4', 'animations3', 'animations2', 'animations1'] //array of frames by name
+        //     }
+        // }
+        console.log(this.atlasData);
+        //debugger;
 
         this.spritesheet = new PIXI.Spritesheet(
             PIXI.BaseTexture.from(this.atlasData.meta.image),
