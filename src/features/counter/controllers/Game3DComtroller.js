464047d495172798876e04358a7158f675085b01
Pngshka <pngshkapng@gmail.com>
2023-11-07 11:44:25 +0300

    small fix

--- src/features/counter/controllers/Game3DComtroller.js
+++ src/features/counter/controllers/Game3DComtroller.js
@@ -1,7 +1,6 @@
-import { GameController } from './GameController'
 import gsap from "gsap";
 
-export default class Game3DController /*extends GameController*/ {
+export default class Game3DController {
     ballController;
     animationController;
     randOXY;
@@ -16,9 +15,7 @@ export default class Game3DController /*extends GameController*/ {
     cube;
     count = 1;
 
-    constructor() {
-        //super();
-    }
+    constructor() {}
 
     static get instance() {
         if (!this._instance) this._instance = new Game3DController();
@@ -28,7 +25,7 @@ export default class Game3DController /*extends GameController*/ {
 
     static _instance = null;
 
-    async loadingManifest() {}
+    async loadingManifest() { }
 
     async initialization() {
         this.container = document.createElement('div');
@@ -45,6 +42,55 @@ export default class Game3DController /*extends GameController*/ {
         this.element.id = 'AddScene';
         document.body.appendChild(this.element);
 
+        const THREE = await import('three');
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.z = 5;
+
+        this.scene.fog = new THREE.Fog('lightblue', 0.01, 3.52);
+        this.scene.background = new THREE.Color('lightblue');
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setClearColor(0x2f3640);
+        this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+
+        this.material = new THREE.ShaderMaterial({
+            vertexShader: `
+              uniform vec3 color1;
+              uniform vec3 color2;
+              varying vec3 vColor;
+            
+              void main() {
+                vec4 globalPosition = modelMatrix * vec4(position, 1.0);
+                vec3 globalPositionXYZ = globalPosition.xyz;
+                
+                vColor = mix(color1, color2, globalPositionXYZ);
+                
+                gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+              }
+            `,
+            fragmentShader: `
+              varying vec3 vColor;
+            
+              void main() {
+                gl_FragColor = vec4(vColor, 1.0);
+              }
+            `
+        });
+
+        this.cube = new THREE.Mesh(this.geometry, this.material);
+        this.cube.position.z = 3;
+        this.cube.position.y = 0;
+        this.cube.position.x = 0;
+
+        this.material.uniforms = {
+            color1: { value: new THREE.Vector3(1, 1, 1) },
+            color2: { value: new THREE.Vector3(1, 0, 0) }
+        };
+
     }
 
     async initLevel() {
@@ -58,6 +104,10 @@ export default class Game3DController /*extends GameController*/ {
                 this.count++;
                 this.loadThreeJS();
             }
+        }.bind(this))
+
+        this.container.addEventListener('click', function () {
+            this.animateObject();
         }.bind(this))
 
     }
@@ -98,79 +148,23 @@ export default class Game3DController /*extends GameController*/ {
         const THREE = await import('three');
         this.container = document.getElementById('CanvasFrame');
 
-        this.scene = new THREE.Scene();
-
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.z = 5;
-
-        {
-            const near = 0.01;
-            const far = 3.52;
-            const color = 'lightblue';
-            this.scene.fog = new THREE.Fog(color, near, far);
-            this.scene.background = new THREE.Color(color);
-        }
-
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setClearColor(0x2f3640);
-        this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-
         this.container.appendChild(this.renderer.domElement);
         this.container.style.position = 'absolute';
         this.container.style.left = 300 + 'px';
 
-        this.geometry = new THREE.BoxGeometry(1, 1, 1);
-
-        this.material = new THREE.ShaderMaterial({
-            vertexShader: `
-              uniform vec3 color1;
-              uniform vec3 color2;
-              varying vec3 vColor;
-            
-              void main() {
-                // Получение глобальной координаты вершины
-                vec4 globalPosition = modelMatrix * vec4(position, 1.0);
-                vec3 globalPositionXYZ = globalPosition.xyz;
-                
-                // Вычисление цвета в зависимости от глобальных координат
-                vColor = mix(color1, color2, globalPositionXYZ);
-                
-                // Позиция вершины
-                gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-              }
-            `,
-            fragmentShader: `
-              varying vec3 vColor;
-            
-              void main() {
-                // Устанавливаем цвет фрагмента
-                gl_FragColor = vec4(vColor, 1.0);
-              }
-            `
-        });
-
         this.elementRemoveScene.addEventListener('click', function () {
-            if (this.count === 1){
+            if (this.count === 1) {
                 this.count--;
                 this.removeThreeJS(this.renderer.domElement);
             }
-          }.bind(this))
+        }.bind(this))
 
-        this.cube = new THREE.Mesh(this.geometry, this.material);
-        this.cube.position.z = 3;
-        this.cube.position.y = 0;
-        this.cube.position.x = 0;
         this.scene.add(this.cube);
-
-        this.material.uniforms = {
-            color1: { value: new THREE.Vector3(1, 1, 1) },
-            color2: { value: new THREE.Vector3(1, 0, 0) }
-        };
 
         this.animateObject();
 
-        this.container.addEventListener('click', function () {
-            this.animateObject();
-        }.bind(this))
+        // this.container.addEventListener('click', function () {
+        //     this.animateObject();
+        // }.bind(this))
     }
 }
